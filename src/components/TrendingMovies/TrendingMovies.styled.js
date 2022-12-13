6f9c11cd78bac7a5b5d1e101a2d280137c5a6b2a
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2022-12-13 17:57:04 +0200

    style trendMovie

--- src/components/TrendingMovies/TrendingMovies.styled.js
+++ src/components/TrendingMovies/TrendingMovies.styled.js
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 export const List = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  gap: 30px 15px;
   list-style: none;
+  margin-left: -30px;
+  margin-top: -30px;
 `;
 
 export const Title = styled.h1`
@@ -14,6 +15,9 @@ export const Title = styled.h1`
 `;
 
 export const Item = styled.li`
+  flex-basis: calc(100% / 4 - 30px);
+  margin-left: 30px;
+  margin-top: 30px;
   box-shadow: 10px 9px 14px -6px rgba(0, 0, 0, 0.75);
   border-radius: ${p => p.theme.radii.normal};
   transition: box-shadow, scale 250ms cubic-bezier(0.075, 0.82, 0.165, 1);
